382cdc990a715ff7e5df78df1486c6ce8030702b
Javier Sánchez Rois <javier.sanchez.rois@gmail.com>
2023-10-05 15:14:45 +0200

    Pintar en la página

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+const drawResult = (text) => {
+    document.getElementById("result").innerHTML = text;
+};
+
+
+
 document.getElementById("run").addEventListener("click", () => {
     let url = document.getElementById("url").value;
 
     fetch(url)        
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => JSON.stringify(data))
+        .then(data => drawResult(data));
 });
